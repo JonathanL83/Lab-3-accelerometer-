dc98055546f266fc2c00f8e1775b8d2bf9362057
JonathanL83 <jonathanlynch@outlook.ie>
2024-08-04 09:10:17 +0000

    Lab -3-accerometer with compute angle function first commit

--- main.cpp
+++ main.cpp
@@ -1,29 +1,26 @@
 #include "mbed.h"
 #include "LSM6DSLSensor.h"
+#include <cmath>
 
 #define PI 3.141592654
 
-static DevI2C devI2c(PB_11, PB_10);
-static LSM6DSLSensor acc_gyro(&devI2c, 0xD4, D4, D5);
+static DevI2C devI2c(PB_11,PB_10);
+static LSM6DSLSensor acc_gyro(&devI2c,0xD4,D4,D5);
 
-void computeAngles(int x, int y, int z, float &pitch, float &roll) {
-    float ax = x * 0.061 * 9.81;
-    float ay = y * 0.061 * 9.81;
-    float az = z * 0.061 * 9.81;
+float computeRoll(int x, int y, int z) {
+    return atan2(y, sqrt(x * x + z * z)) * 180.0 / PI;
+}
 
-    pitch = atan2(ay, sqrt(ax * ax + az * az));
-    roll = atan2(-ax, az);
-
-    pitch = pitch * 180.0 / PI;
-    roll = roll * 180.0 / PI;
+float computePitch(int x, int y, int z) {
+    return atan2(x, sqrt(y * y + z * z)) * 180.0 / PI;
 }
 
 int main() {
     uint8_t id;
     int32_t axes[3];
-    float pitch = 0, roll = 0;
-
+    float roll, pitch;
     acc_gyro.init(NULL);
+
     acc_gyro.enable_x();
     acc_gyro.enable_g();
 
@@ -31,10 +28,11 @@ int main() {
     acc_gyro.read_id(&id);
     printf("LSM6DSL accelerometer & gyroscope = 0x%X\r\n", id);
 
-    while (1) {
+    while(1) {
         acc_gyro.get_x_axes(axes);
-        computeAngles(axes[0], axes[1], axes[2], pitch, roll);
-        printf("LSM6DSL: X=%6d, Y=%6d, Z=%6d, Pitch=%3.2f, Roll=%3.2f\r\n", axes[0], axes[1], axes[2], pitch, roll);
+        roll = computeRoll(axes[0], axes[1], axes[2]);
+        pitch = computePitch(axes[0], axes[1], axes[2]);
+        printf("LSM6DSL: %6d, %6d, %6d, roll: %3.2f, pitch: %3.2f\r\n", axes[0], axes[1], axes[2], roll, pitch);
 
         thread_sleep_for(2000);
     }
